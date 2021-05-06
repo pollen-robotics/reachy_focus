e12f1b5af4b568abbe8f35e8d9271d0892a24c4a
marjoriePaillet <marjoriepaillet@live.fr>
2021-05-06 16:44:35 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -11,18 +11,25 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'zoom_kurokesu>=1',
+        'pynput',
+        'cv2',
+    ],
     zip_safe=True,
     maintainer='nuc2',
     maintainer_email='glannuzel@ensc.fr',
-    description='TODO: Package description',
+    description='ROS2 Auto-focus algorithm for Reachy',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'camera_focus = reachy_focus.camera_focus:main',
+            'focus = reachy_focus.focus:main',
             'camera_focus_1traitement = reachy_focus.camera_focus_1traitement:main',
             'view_cam = reachy_focus.view_cam:main',
+            'camera_zoom_client = reachy_focus.camera_zoom_client:main',
         ],
     },
 )
