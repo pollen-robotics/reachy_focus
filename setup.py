097bf9b7a47aff7cc5a32fbcbbbd804698ceea32
marjoriePaillet <marjoriepaillet@live.fr>
2021-05-07 15:45:38 +0200

    Adding camera_focus launch file and renaming files.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'reachy_focus'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=[
         'setuptools',
@@ -25,9 +28,6 @@ setup(
     entry_points={
         'console_scripts': [
             'camera_focus = reachy_focus.camera_focus:main',
-            'focus = reachy_focus.focus:main',
-            'camera_focus_1traitement = reachy_focus.camera_focus_1traitement:main',
-            'view_cam = reachy_focus.view_cam:main',
             'camera_zoom_client = reachy_focus.camera_zoom_client:main',
         ],
     },
