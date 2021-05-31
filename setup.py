371bc38104a41fe64ad8e93befc53c86f004c6cc
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2021-05-31 12:18:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,12 @@ setup(
     zip_safe=True,
     maintainer='nuc2',
     maintainer_email='contact@pollen-robotics.com',
-    description='ROS2 Auto-focus algorithm for Reachy',
+    description='ROS2 autofocus algorithm for Reachy',
     license='Apache-2.0 License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'camera_focus = reachy_focus.camera_focus:main',
-            'camera_zoom_client = reachy_focus.camera_zoom_client:main',
         ],
     },
 )
